fd622540e6c701dbef3cbe0f01baff90e9d0666d
Gamer1120 <Gamer1120@github.com>
2022-12-27 23:32:59 +0100

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,30 @@
 import { useState, useEffect } from "react";
-import { L, useMap, MapContainer } from "react-leaflet";
+import {
+  L,
+  useMap,
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+} from "react-leaflet";
 
 export default function Map() {
   return (
     <MapContainer
+      id="map"
       center={[51.505, -0.09]}
-      zoom={20}
-      style={{ height: "100vh" }}
-    ></MapContainer>
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }

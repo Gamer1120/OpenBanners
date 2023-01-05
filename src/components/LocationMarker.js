21ebb6c20cc7c68a1d50a6018dcaba11d1e3471b
Gamer1120 <michaelkoopmantest@gmail.com>
2023-01-05 11:46:17 +0100

    Save previous position

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -7,44 +7,30 @@ import icon, { locationIcon } from "../constants";
 
 export default function LocationMarker() {
   const [position, setPosition] = useState(null);
-  const [bbox, setBbox] = useState([]);
 
   const map = useMap();
 
   const [time, setTime] = useState(Date.now());
 
-  useEffect (() => {
-    console.log('runnin')
-    const interval = setInterval(() => {setTime(Date.now())
-      map.locate().on("locationfound", function (e) {
-        console.log('location found!')
-        setPosition(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
-        // setBbox(e.bounds.toBBoxString().split(","));
-      })}, 250);
+  useEffect(() => {
+    let previousPosition = null;
+    map.locate().on("locationfound", function (e) {
+      console.log("triggering event hook");
+      console.log("Previous position " + previousPosition);
+      previousPosition = e.latlng;
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    });
+    const interval = setInterval(() => {
+      setTime(Date.now());
+      map.locate();
+    }, 5000);
     return () => {
       clearInterval(interval);
     };
-  }, [])
-
-  // useEffect(() => {
-  //   map.locate().on("locationfound", function (e) {
-  //     setPosition(e.latlng);
-  //     map.flyTo(e.latlng, map.getZoom());
-  //     // setBbox(e.bounds.toBBoxString().split(","));
-  //   });
-  // }, [map, position]);
+  }, []);
 
   return position === null ? null : (
-    <Marker position={position} icon={locationIcon}>
-      {/* <Popup>
-        You are here. <br />
-        Map bbox: <br />
-        <b>Southwest lng</b>: {bbox[0]} <br />
-        <b>Southwest lat</b>: {bbox[1]} <br />
-        <b>Northeast lng</b>: {bbox[2]} <br />
-        <b>Northeast lat</b>: {bbox[3]}
-      </Popup> */}
-    </Marker>
+    <Marker position={position} icon={locationIcon} />
   );
 }

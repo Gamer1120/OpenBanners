6df4dae6359d6327947c6329af3b9a68b263fffd
Gamer1120 <michaelkoopmantest@gmail.com>
2023-01-05 11:51:43 +0100

    Start working on angle

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -12,14 +12,35 @@ export default function LocationMarker() {
 
   const [time, setTime] = useState(Date.now());
 
+  function angleFromCoordinate(lat1, lon1, lat2, lon2) {
+    var p1 = {
+      x: lat1,
+      y: lon1,
+    };
+
+    var p2 = {
+      x: lat2,
+      y: lon2,
+    };
+    // angle in radians
+    var angleRadians = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+    // angle in degrees
+    var angleDeg = (Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180) / Math.PI;
+    console.log(angleDeg);
+    return angleDeg;
+  }
+
   useEffect(() => {
     let previousPosition = null;
     map.locate().on("locationfound", function (e) {
-      console.log("triggering event hook");
-      console.log("Previous position " + previousPosition);
-      previousPosition = e.latlng;
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
+      if (
+        previousPosition == null ||
+        (previousPosition.lat !== e.lat && previousPosition.lng !== e.lng)
+      ) {
+        previousPosition = e.latlng;
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      }
     });
     const interval = setInterval(() => {
       setTime(Date.now());

bb66e6acff11cec4335ec0bdd25751bc4a5861e7
Gamer1120 <michaelkoopmantest@gmail.com>
2023-06-07 10:56:26 +0200

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Map from "./components/Map.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <Map />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/banner" element={<Map />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <p>This is the home page.</p>;
+}

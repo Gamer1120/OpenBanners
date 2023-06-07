d980089be8b8d4bca424c6012c9505ff84035dbb
Gamer1120 <michaelkoopmantest@gmail.com>
2023-06-07 13:23:16 +0200

    Add shadow

--- src/components/MissionStepMarker.js
+++ src/components/MissionStepMarker.js
@@ -10,11 +10,14 @@ export default function MissionStepMarker({
   isFirst,
 }) {
   const iconContentClass = isFirst ? "first-icon" : "";
+  const shadowStyle = isFirst
+    ? "box-shadow: 0px 0px 8px rgba(0, 0, 0, 1);"
+    : "";
   const iconHtml = `<div class="icon-content ${iconContentClass}" style="height: ${
     isFirst ? "24px" : "12px"
   }; width: ${isFirst ? "24px" : "12px"}; border-radius: ${
     isFirst ? "50%" : "50%"
-  }; background-color: ${isFirst ? color : "#000000"}">${
+  }; background-color: ${isFirst ? color : "#000000"};${shadowStyle}">${
     isFirst ? missionNumber : ""
   }</div>`;
 

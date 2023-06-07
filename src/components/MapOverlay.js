c494c91a0c5f89fe4f90bf9820760827f746365a
Gamer1120 <michaelkoopmantest@gmail.com>
2023-06-07 14:54:38 +0200

    Prevent mouse events from propagating to map

--- src/components/MapOverlay.js
+++ src/components/MapOverlay.js
@@ -1,14 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function MapOverlay({ missionCount }) {
   const [currentMission, setCurrentMission] = useState(1);
+  const overlayRef = useRef(null);
 
   const handleIncrement = () => {
     setCurrentMission((prevMission) => prevMission + 1);
   };
 
+  useEffect(() => {
+    const handleOverlayMouseEvents = (event) => {
+      event.stopPropagation(); // Stop the propagation of all mouse events
+    };
+
+    if (overlayRef.current) {
+      overlayRef.current.addEventListener(
+        "mousedown",
+        handleOverlayMouseEvents
+      );
+      overlayRef.current.addEventListener("mouseup", handleOverlayMouseEvents);
+      overlayRef.current.addEventListener("click", handleOverlayMouseEvents);
+      overlayRef.current.addEventListener("dblclick", handleOverlayMouseEvents);
+    }
+
+    return () => {
+      if (overlayRef.current) {
+        overlayRef.current.removeEventListener(
+          "mousedown",
+          handleOverlayMouseEvents
+        );
+        overlayRef.current.removeEventListener(
+          "mouseup",
+          handleOverlayMouseEvents
+        );
+        overlayRef.current.removeEventListener(
+          "click",
+          handleOverlayMouseEvents
+        );
+        overlayRef.current.removeEventListener(
+          "dblclick",
+          handleOverlayMouseEvents
+        );
+        // Remove more mouse events as needed
+      }
+    };
+  }, []);
+
   return (
-    <div className="overlay">
+    <div ref={overlayRef} className="overlay">
       <div className="overlay-controls">
         <button className="overlay-button-minus">-</button>
         <p>

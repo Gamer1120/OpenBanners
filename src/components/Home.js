831be921ccbd4af73e181e19ef8129e7f50ae3e9
Gamer1120 <michaelkoopmantest@gmail.com>
2023-06-08 13:44:02 +0200

    Continue making home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,138 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  TextField,
+  IconButton,
+  InputAdornment,
+} from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import { Explore, LocationOn, Search } from "@mui/icons-material";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: theme.palette.grey[900],
+    minHeight: "100vh",
   },
   appBar: {
-    backgroundColor: theme.palette.background.default,
+    backgroundColor: theme.palette.grey[800],
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
+  titleContainer: {
+    display: "flex",
+    alignItems: "center",
+    width: "33%",
+  },
   title: {
-    flexGrow: 1,
+    marginLeft: theme.spacing(2),
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "33%",
+  },
+  searchContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    width: "33%",
+  },
+  searchInput: {
+    paddingRight: theme.spacing(2),
   },
   section: {
     marginTop: theme.spacing(2),
+    color: theme.palette.common.white,
+  },
+  permissionText: {
+    color: theme.palette.error.main,
   },
 }));
 
 export default function Home() {
   const classes = useStyles();
+  const [permissionStatus, setPermissionStatus] = useState("unknown");
+
+  useEffect(() => {
+    const handlePermission = (status) => {
+      setPermissionStatus(status);
+      if (status === "granted") {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            console.log("Latitude:", latitude);
+            console.log("Longitude:", longitude);
+          },
+          (error) => {
+            console.error("Error getting location:", error);
+          }
+        );
+      }
+    };
+
+    if ("permissions" in navigator) {
+      navigator.permissions.query({ name: "geolocation" }).then((result) => {
+        handlePermission(result.state);
+        result.onchange = () => {
+          handlePermission(result.state);
+        };
+      });
+    } else if ("geolocation" in navigator) {
+      handlePermission("granted");
+    } else {
+      handlePermission("denied");
+    }
+  }, []);
 
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            OpenBanners
-          </Typography>
-          <Button color="inherit">Browse</Button>
-          <Button color="inherit">Map</Button>
+          <Container className={classes.titleContainer}>
+            <Typography variant="h6" className={classes.title}>
+              OpenBanners
+            </Typography>
+          </Container>
+          <Container className={classes.buttonContainer}>
+            <Button color="inherit" startIcon={<Explore />} disableElevation>
+              Browse
+            </Button>
+            <Button color="inherit" startIcon={<LocationOn />} disableElevation>
+              Map
+            </Button>
+          </Container>
+          <Container className={classes.searchContainer}>
+            <TextField
+              variant="outlined"
+              placeholder="Search"
+              size="small"
+              className={classes.searchInput}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton>
+                      <Search />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Container>
         </Toolbar>
       </AppBar>
       <Container className={classes.section}>
         <Typography variant="h5">Banners near me</Typography>
+        {permissionStatus !== "granted" && (
+          <Typography variant="body2" className={classes.permissionText}>
+            Location Permission not allowed
+          </Typography>
+        )}
       </Container>
     </div>
   );

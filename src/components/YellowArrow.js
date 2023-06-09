7ef82a7a7acb837a82401dd45f639e51c4081796
Gamer1120 <michaelkoopmantest@gmail.com>
2023-06-09 16:19:29 +0200

    Better arrow

--- src/components/YellowArrow.js
+++ src/components/YellowArrow.js
@@ -5,7 +5,7 @@ const YellowArrow = ({ direction }) => {
     position: "absolute",
     top: "10px",
     left: "50%",
-    transform: "translateX(-50%)",
+    transform: "translateX(-50%) rotate(180deg)", // Added rotate(180deg) to flip the arrow
     zIndex: "1000",
   };
 
@@ -13,15 +13,16 @@ const YellowArrow = ({ direction }) => {
     <div style={arrowStyle}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
-        viewBox="0 0 32 32"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
       >
         <path
-          fill="transparent"
-          d="M15.94 5.28l-11.3 12.2 2.84 2.92 5.46-5.5v17.1h3v-17.07l5.45 5.48 2.84-2.9z"
+          fill="none"
+          stroke="#F8C60B"
+          strokeWidth="2" // Changed stroke-width to strokeWidth
+          d="M12 22L1 11h7V2h8v9h7z"
         />
-        <path fill="yellow" d="M16 2l-16 16h9v14h14v-14h9z" />
       </svg>
     </div>
   );

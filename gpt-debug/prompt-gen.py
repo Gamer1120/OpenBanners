e41ac5bbe58e39d6fcab7cd3ef26550b099d9fc5
Gamer1120 <michaelkoopmantest@gmail.com>
2023-06-09 14:39:57 +0200

    Increase z-index

--- gpt-debug/prompt-gen.py
+++ gpt-debug/prompt-gen.py
@@ -24,6 +24,6 @@ for file in files:
         output += f.read() + "\n"
         output += "```\n\n"
 
-output += "Do not use comments in any generated code. Please do not leave out code fragments with {...}.\n\n"
+output += "Do not use comments in any generated code. Please do not leave out code fragments with {...}. Always provide the full file when generating code, so I can copy-paste it.\n\n"
 print(output)
 pyperclip.copy(output)

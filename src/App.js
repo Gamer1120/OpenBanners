a0b4ca77358081795f6b3fcea0bc1a8863cdcbd3
Gamer1120 <michaelkoopmantest@gmail.com>
2023-06-10 22:03:09 +0200

    Add /browse/ path

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route exact path="/browse/" component={Home} />
+          <Route exact path="/browse/" element={<Home />} />
           <Route path="/banner/:bannerId" element={<Map />} />
         </Routes>
       </Router>

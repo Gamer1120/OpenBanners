c4da9f2e3b956c480e07260956102a2bc5cb6e35
Gamer1120 <michaelkoopmantest@gmail.com>
2023-06-21 10:59:33 +0200

    Fix zoom on BannerDetailsPage

--- src/components/BannerDetailsPage.js
+++ src/components/BannerDetailsPage.js
@@ -2,9 +2,10 @@ import { MapContainer, TileLayer } from "react-leaflet";
 import LocationMarker from "./LocationMarker";
 import BannerMarkers from "./BannerMarkers";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import BannerDetailsCard from "./BannerDetailsCard";
 import BannerInfo from "./BannerInfo";
+import L from "leaflet";
 
 export default function BannerDetailsPage() {
   const { bannerId } = useParams();
@@ -12,8 +13,9 @@ export default function BannerDetailsPage() {
   const location = useLocation(); // Access the current location
 
   const [currentMission, setCurrentMission] = useState(0);
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const mapRef = useRef(null);
 
   useEffect(() => {
     fetch(`https://api.bannergress.com/bnrs/${bannerId}`)
@@ -21,8 +23,6 @@ export default function BannerDetailsPage() {
       .then(
         (result) => {
           setItems(result);
-          console.log("items");
-          console.log(result);
           setIsLoading(false);
         },
         (error) => {
@@ -43,6 +43,30 @@ export default function BannerDetailsPage() {
     }
   }, [location]);
 
+  useEffect(() => {
+    if (!isLoading && items.missions) {
+      const missionCoordinates = Object.values(items.missions)
+        .map((mission) => {
+          const { poi } = mission.steps[0];
+          const latitude = poi.latitude;
+          const longitude = poi.longitude;
+          if (latitude && longitude) {
+            return [latitude, longitude];
+          }
+          return null;
+        })
+        .filter((coord) => coord !== null);
+
+      if (missionCoordinates.length > 0) {
+        const bounds = L.latLngBounds(missionCoordinates);
+        mapRef.current?.fitBounds(bounds, {
+          padding: [50, 50],
+          animate: true,
+        });
+      }
+    }
+  }, [isLoading, items.missions]);
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -60,13 +84,14 @@ export default function BannerDetailsPage() {
           zoom={8}
           scrollWheelZoom={true}
           style={{ height: "100vh" }}
+          ref={mapRef}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors. This website is NOT affiliated with Bannergress in any way!'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <BannerMarkers
-            missions={Object.values(items.missions)}
+            missions={items.missions ? Object.values(items.missions) : []}
             currentMission={currentMission}
           />
           <LocationMarker />
